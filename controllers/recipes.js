bc799a649841066365ab651440f4a0a5793a3ba6
ebrahimalarayedh <ibrahim.mhd@hotmail.com>
2025-05-17 00:36:37 +0300

    Done both Parts

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -7,74 +7,124 @@ const Ingredient = require("../models/ingredient")
 const Recipe = require("../models/recipe")
 
 //index
-router.get("/", async (req,res)=>{
-    try{
-        
-        res.render('recipes/index.ejs')
-    }catch(err){
+router.get("/", async (req, res) => {
+    const user = req.session.user
+    try {
+        const allMyRecipes = await Recipe.find({ owner: user._id }).populate('ingredients');
+        res.render('recipes/index.ejs', { recipes: allMyRecipes });
 
+    } catch (err) {
+        console.error('Error fetching user recipes:', err);
+        res.status(500).send('Internal Server Error');
     }
 })
 
 //new
-router.get("/new", async (req,res)=>{
-    try{
+router.get("/new", async (req, res) => {
+    try {
         const fetchedIngredients = await Ingredient.find()
-        res.render('recipes/new.ejs', {ingredients: fetchedIngredients})
-    }catch(err){
-        
+        res.render('recipes/new.ejs', { ingredients: fetchedIngredients })
+    } catch (err) {
+
     }
 })
 
 //create
-router.post("/", async (req,res)=>{
+router.post("/", async (req, res) => {
     console.log(req.body)
-    try{
+    try {
         const createdRecipe = await Recipe.create({
             name: req.body.name,
             instructions: req.body.instructions,
             owner: req.session.user,
-            ingredients:  Array.isArray(req.body.ingredients) ? req.body.ingredients : [req.body.ingredients]
+            ingredients: Array.isArray(req.body.ingredients) ? req.body.ingredients : [req.body.ingredients]
         })
         res.redirect('/recipes')
-    }catch(err){
-            res.status(400).send('Error creating recipe')        
+    } catch (err) {
+        res.status(400).send('Error creating recipe')
     }
 })
 
 //show
-router.get("/:recipeId", async (req,res)=>{
-    try{
-
-    }catch(err){
-        
+router.get("/:recipeId", async (req, res) => {
+    const { recipeId } = req.params;
+    try {
+        const recipe = await Recipe.findById(recipeId).populate("ingredients");
+        if (!recipe) return res.status(404).send("Recipe not found");
+        res.render("recipes/show.ejs", { recipe });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server error");
     }
 })
 
 //edit
-router.get("/:recipeId/edit", async (req,res)=>{
-    try{
+router.get("/:recipeId/edit", async (req, res) => {
+    const { recipeId } = req.params;
+    const user = req.session.user;
 
-    }catch(err){
-        
+    try {
+        const recipe = await Recipe.findById(recipeId).populate("ingredients owner");
+
+        if (!recipe) {
+            return res.status(404).send("Recipe not found.");
+        }
+
+        if (!recipe.owner._id.equals(user._id)) {
+            return res.status(403).send("Unauthorized to edit this recipe.");
+        }
+
+        const allIngredients = await Ingredient.find({});
+        res.render("recipes/edit.ejs", { recipe, allIngredients });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error loading edit form.");
     }
 })
 
 //update
-router.put("/:recipeId", async (req,res)=>{
-    try{
+router.put("/:recipeId", async (req, res) => {
+    try {
+        const { recipeId } = req.params;
+        const { name, instructions, ingredients } = req.body;
+        const user = req.session.user;
 
-    }catch(err){
-        
+        const recipe = await Recipe.findById(recipeId);
+        if (!recipe) return res.status(404).send("Recipe not found");
+        if (recipe.owner.toString() !== user._id.toString()) {
+            return res.status(403).send("Unauthorized");
+        }
+
+        recipe.name = name;
+        recipe.instructions = instructions;
+        recipe.ingredients = Array.isArray(ingredients) ? ingredients : [ingredients];
+        await recipe.save();
+
+        res.redirect(`/recipes/${recipeId}`);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server error");
     }
 })
 
 //delete
-router.delete("/:recipeId", async (req,res)=>{
-    try{
+router.delete("/:recipeId", async (req, res) => {
+    try {
+        const { recipeId } = req.params;
+        const user = req.session.user;
 
-    }catch(err){
-        
+        const recipe = await Recipe.findById(recipeId);
+        if (!recipe) return res.status(404).send("Recipe not found");
+        if (recipe.owner.toString() !== user._id.toString()) {
+            return res.status(403).send("Unauthorized");
+        }
+
+        await recipe.deleteOne();
+        res.redirect("/recipes");
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server error");
     }
 })
 
